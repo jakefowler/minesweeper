3d6db74293d30c3919c93ebd2c417ab1cb850748
Ezekiel Newren <ezekielnewren@gmail.com>
2019-06-05 16:33:59 -0600

    added a mysql download function

---
+++ authentication/newUser.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "student";
+$password = "weber";
+$dbname = "se2";
+
+function download($query) {
+	global $servername, $username, $password, $dbname;
+
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$stmt = $conn->prepare($query);
+		$stmt->execute();
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$data = $stmt->fetchAll();
+		
+		return $data;
+	} catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	} finally {
+		$conn = null;
+	}
+}
+
+?>
